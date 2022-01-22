553de750064754364e46cbad9917575c5dd67a14
risolij <risolij@gmail.com>
2022-01-21 23:19:09 -0700

    playing with traits && state

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::io;
-use tui::Terminal;
-use tui::backend::{TermionBackend, Backend};
-use tui::widgets::{Block, Borders};
 use termion::raw::IntoRawMode;
-use tui::widgets::Tabs;
-use tui::layout::{Constraint, Layout, Direction};
-use tui::text::Spans;
+use tui::backend::{Backend, TermionBackend};
+use tui::layout::{Constraint, Direction, Layout};
 use tui::style::{Color, Style};
+use tui::text::Spans;
+use tui::widgets::Tabs;
+use tui::widgets::{Block, Borders};
+use tui::Terminal;
 
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
@@ -17,7 +17,7 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
-fn run<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()>{
+fn run<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
     terminal.autoresize().unwrap();
     terminal.clear().unwrap();
     terminal.draw(|f| {
@@ -29,7 +29,8 @@ fn run<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()>{
                     Constraint::Percentage(15),
                     Constraint::Percentage(75),
                     Constraint::Percentage(10),
-                ].as_ref()
+                ]
+                .as_ref(),
             )
             .split(f.size());
 
@@ -39,14 +40,12 @@ fn run<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()>{
         let block = generate_block("Block 2", true);
         f.render_widget(block, chunks[1]);
 
-
         let block = generate_block("Block 3", true);
         f.render_widget(block, chunks[2]);
     })?;
 
     Ok(())
 }
-
 
 fn header() -> Tabs<'static> {
     let titles = ["Tab1", "Tab2", "Tab3"]
@@ -62,12 +61,40 @@ fn header() -> Tabs<'static> {
 
 fn generate_block(title: &'static str, borders: bool) -> Block {
     if borders {
-        return Block::default().title(title).borders(Borders::ALL)
+        return Block::default().title(title).borders(Borders::ALL);
     };
 
-    return Block::default().title(title)
+    return Block::default().title(title);
 }
 
+trait State {}
+
+struct ReadFile {
+    state: Option<Box<dyn State>>,
+    contents: String,
+}
+
+impl ReadFile {
+    fn new(file: &'static str) -> Self {
+	    match std::fs::read_to_string(file.to_string()) {
+	        Ok(f) => {
+	    	    Self {
+	    	        state: Some(Box::new(Read {})),
+	    	        contents: f,
+	    	    }
+	        },
+            Err(e) => {
+                println!("uh oh {}", e);
+                std::process::exit(1);
+            }
+	    }
+    }
+}
+
+struct Read {}
+impl State for Read {}
+
+  
 
 #[derive(Debug)]
 struct CpuInfo {
@@ -79,44 +106,32 @@ struct CpuInfo {
 
 impl Default for CpuInfo {
     fn default() -> Self {
-        match std::fs::read_to_string("/proc/cpuinfo") {
-            Ok(file) => {
-                let processors = file
-                    .lines()
-                    .filter(|f| f.contains("processor"))
-                    .count();
+        let file = ReadFile::new("/proc/cpuinfo");
 
-                let vendor_id = file
-                    .lines()
-                    .filter(|f| f.contains("vendor_id"))
-                    .map(|f| f.to_string())
-                    .take(1)
-                    .collect::<String>();
+        let model_name = file
+            .contents
+            .lines()
+            .filter(|f| f.contains("model name"))
+            .map(|f| f.to_string())
+            .take(1)
+            .collect::<String>();
 
-                let model_name = file
-                    .lines()
-                    .filter(|f| f.contains("model name"))
-                    .map(|f| f.to_string())
-                    .take(1)
-                    .collect::<String>();
+        let vendor_id = file
+            .contents
+            .lines()
+            .filter(|f| f.contains("vendor_id"))
+            .map(|f| f.to_string())
+            .take(1)
+            .collect::<String>();
 
-                let cores = file
-                    .lines()
-                    .filter(|f| f.contains("cpu cores"))
-                    .count();
+        let processors = file.contents.lines().filter(|f| f.contains("processor")).count();
+        let cores = file.contents.lines().filter(|f| f.contains("cpu cores")).count();
 
-                Self {
-                    model_name, 
-                    vendor_id, 
-                    cores,
-                    processors,
-                }
-            },
-            Err(e) => {
-                println!("Uh oh {}", e);
-                std::process::exit(1);
-            }
+        Self {
+            model_name,
+            vendor_id,
+            cores,
+            processors,
         }
     }
-
 }
